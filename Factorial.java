058f19599e79fc2cf5366f1babc2df9ef6334582
b-Kesavaram <114815559+b-Kesavaram@users.noreply.github.com>
2022-10-01 19:01:33 +0530

    Add files via upload
    Week 1 Day 1 Assignments

---
+++ Factorial.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int j =1;
+		int fact =5;
+				for ( int i=1;i<=fact;i++) {
+					j = j * i;
+				}
+					System.out.println("Factorial of " + fact +" is "  +  j);	
+		
+}
+}
